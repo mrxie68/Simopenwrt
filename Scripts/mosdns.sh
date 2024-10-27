6f4508ce712eca0dc5f0f924e85cdc961bfb14ad
mrxie68 <mrxie6@gmail.com>
2024-10-27 23:05:06 +0800

    Add new content

--- Scripts/mosdns.sh
+++ Scripts/mosdns.sh
@@ -2,9 +2,13 @@ rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
+rm -rf feeds/packages/net/tailscale
+git clone https://github.com/tailscale/tailscale.git feeds/packages/net/tailscale
+
 if [[ $WRT_TARGET == *"IPQ"* ]]; then
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 ./scripts/feeds update istore
 ./scripts/feeds install -d y -p istore luci-app-store
 fi
+
